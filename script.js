29425c87866ffd1fe6c162119783d213d98653e0
nazid <nazidk773@gmail.com>
2024-11-25 08:47:41 +0530

    return sum

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// Create a function that takes two numbers as arguments and returns their sum.
+
+function addition(a,b){
+    return a+b;
+}
+
+console.log(addition(3, 2));  //➞ 5
+console.log(addition(-3, -6));  //➞ -9
+console.log(addition(7, 3));  //➞ 10
+
+ 
