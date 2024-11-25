96fb00a891fd873721f5916cea4160c8697cc305
nazid <nazidk773@gmail.com>
2024-11-25 08:57:13 +0530

    calculate age

--- script.js
+++ script.js
@@ -12,10 +12,21 @@
  
 
 // Q2: Create a function that takes a number as an argument, increments the number by +1 and returns the result.
-function increment(num){
-    return num+1;
+// function increment(num){
+//     return num+1;
+// }
+
+// console.log(increment(0)); //➞ 1
+// console.log(increment(9)); //➞ 10
+// console.log(increment(-3)); //➞ -2
+
+
+
+// Q3: Create a function that takes the age in years and returns the age in days.
+function calcAge(num){
+    return num * 365;
 }
 
-console.log(increment(0)); //➞ 1
-console.log(increment(9)); //➞ 10
-console.log(increment(-3)); //➞ -2
+console.log(calcAge(65)); //➞ 23725
+console.log(calcAge(0)); //➞ 0
+console.log(calcAge(20)); //➞ 7300
