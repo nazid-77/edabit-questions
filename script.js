6162a743c409cdc77f0e5666cd13f15dd356272c
nazid <nazidk773@gmail.com>
2024-11-25 08:53:21 +0530

    return increment

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+// Algebra questions
 
-// Create a function that takes two numbers as arguments and returns their sum.
+// Q1: Create a function that takes two numbers as arguments and returns their sum.
+// function addition(a,b){
+//     return a+b;
+// }
 
-function addition(a,b){
-    return a+b;
-}
-
-console.log(addition(3, 2));  //➞ 5
-console.log(addition(-3, -6));  //➞ -9
-console.log(addition(7, 3));  //➞ 10
+// console.log(addition(3, 2));  //➞ 5
+// console.log(addition(-3, -6));  //➞ -9
+// console.log(addition(7, 3));  //➞ 10
 
  
+
+// Q2: Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+function increment(num){
+    return num+1;
+}
+
+console.log(increment(0)); //➞ 1
+console.log(increment(9)); //➞ 10
+console.log(increment(-3)); //➞ -2
