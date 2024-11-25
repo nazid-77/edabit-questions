767a828f8cfa1215f316796142b9d89b98b1d6d4
nazid <nazidk773@gmail.com>
2024-11-25 09:15:15 +0530

    calculate points

--- script.js
+++ script.js
@@ -34,10 +34,32 @@
 
 
 // Q:4 create a function for generate square of number
-function square(num){
-    return num * num;
+// function square(num){
+//     return num * num;
+// }
+
+// console.log(square(5)); //➞ 25
+// console.log(square(9)); //➞ 81
+// console.log(square(100)); //➞ 10000
+
+
+
+// Q5:Create a function that takes the number of wins, draws and losses and calculates the number of points a football team has obtained so far.
+// wins get 3 points
+// draws get 1 point
+// losses get 0 points
+
+function footballPoints(wins, draws, losses){
+    return wins*3 + draws*1 + losses*0;
+    
 }
 
-console.log(square(5)); //➞ 25
-console.log(square(9)); //➞ 81
-console.log(square(100)); //➞ 10000
+console.log(footballPoints(3, 4, 2)); //➞ 13
+console.log(footballPoints(5, 0, 2)); //➞ 15
+console.log(footballPoints(0, 0, 1)); //➞ 0
+
+
+
+
+
+
