09bf6d264d8c029d047f7e190c6c5a89439b0087
nazid <nazidk773@gmail.com>
2024-11-26 16:59:54 +0530

    Nth Star Number

--- script.js
+++ script.js
@@ -191,23 +191,38 @@
 // Q14: The 50-30-20 strategy is a simple way to budget, which involves spending 50% of after-tax income on needs, 30% after tax income on wants, and 20% after-tax income on savings or paying off debt.
 // Given the after-tax income as ati, what you are supposed to do is to make a function that will return an object that shows how much a person needs to spend on needs, wants, and savings.
 
-function fiftyThirtyTwenty(ati) {
-    return {
-        Needs: Math.floor(ati * 0.50),
-        Wants: Math.floor(ati*0.30),
-        Savings: Math.floor(ati*0.20),
-    };
+// function fiftyThirtyTwenty(ati) {
+//     return {
+//         Needs: Math.floor(ati * 0.50),
+//         Wants: Math.floor(ati*0.30),
+//         Savings: Math.floor(ati*0.20),
+//     };
+// }
+// console.log(fiftyThirtyTwenty(10000)); //➞ { "Needs": 5000, "Wants": 3000, "Savings": 2000 }
+// console.log(fiftyThirtyTwenty(50000)); //➞ { "Needs": 25000, "Wants": 15000, "Savings": 10000 }
+// console.log(fiftyThirtyTwenty(13450)); //➞ { "Needs": 6725, "Wants": 4035, "Savings": 2690 }
+
+
+
+
+
+// Q15:Create a function that takes a positive integer n and returns the nth "star number".
+// A star number is a centered figurate number that represents a centered hexagram (six-pointed star), such as the one that Chinese checkers is played on. star numbers for 1, 2 and 3
+function starNumber(n){
+    return  6 * n *( n - 1 ) + 1;
 }
 
-console.log(fiftyThirtyTwenty(10000)); //➞ { "Needs": 5000, "Wants": 3000, "Savings": 2000 }
-console.log(fiftyThirtyTwenty(50000)); //➞ { "Needs": 25000, "Wants": 15000, "Savings": 10000 }
-console.log(fiftyThirtyTwenty(13450)); //➞ { "Needs": 6725, "Wants": 4035, "Savings": 2690 }
+console.log(starNumber(2)); //➞ 13
+// n = 2
+// 2nd star number = 13
 
+console.log(starNumber(3)); //➞ 37
+// n = 3
+// 3rd star number = 37
 
-
-
-
-
+console.log(starNumber(5)); //➞ 121
+// n = 5
+// 5th star number = 121
 
 
 
