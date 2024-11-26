803e000110fb6380b4b87fdcd3dec478859ad643
nazid <nazidk773@gmail.com>
2024-11-26 16:53:10 +0530

    50-30-20 Strategy

--- script.js
+++ script.js
@@ -177,18 +177,31 @@
 // The Rubik's cube of side length 2 has 24 stickers.
 // The Rubik's cube of side length 3 has 54 stickers.
 
-function howManyStickers(num){
-    return num * (num * 6);
+// function howManyStickers(num){
+//     return num * (num * 6);
+// }
+// console.log(howManyStickers(1)); //➞ 6
+// console.log(howManyStickers(2)); //➞ 24
+// console.log(howManyStickers(3)); //➞ 54
+
+
+
+
+
+// Q14: The 50-30-20 strategy is a simple way to budget, which involves spending 50% of after-tax income on needs, 30% after tax income on wants, and 20% after-tax income on savings or paying off debt.
+// Given the after-tax income as ati, what you are supposed to do is to make a function that will return an object that shows how much a person needs to spend on needs, wants, and savings.
+
+function fiftyThirtyTwenty(ati) {
+    return {
+        Needs: Math.floor(ati * 0.50),
+        Wants: Math.floor(ati*0.30),
+        Savings: Math.floor(ati*0.20),
+    };
 }
-console.log(howManyStickers(1)); //➞ 6
-console.log(howManyStickers(2)); //➞ 24
-console.log(howManyStickers(3)); //➞ 54
 
-
-
-
-
-
+console.log(fiftyThirtyTwenty(10000)); //➞ { "Needs": 5000, "Wants": 3000, "Savings": 2000 }
+console.log(fiftyThirtyTwenty(50000)); //➞ { "Needs": 25000, "Wants": 15000, "Savings": 10000 }
+console.log(fiftyThirtyTwenty(13450)); //➞ { "Needs": 6725, "Wants": 4035, "Savings": 2690 }
 
 
 
